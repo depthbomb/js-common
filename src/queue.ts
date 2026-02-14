44679e64931104a22ea46d74feb095e8844ddc83
depthbomb <depthbomb@users.noreply.github.com>
2026-02-14 14:36:49 -0600

    perf(queue): make queue dequeue O(1) amortized

--- src/queue.ts
+++ src/queue.ts
@@ -1,5 +1,6 @@
 export class Queue<T> {
 	private items: T[] = [];
+	private head = 0;
 
 	public constructor(initial?: Iterable<T>) {
 		if (initial) {
@@ -11,14 +12,14 @@ export class Queue<T> {
 	 * Get the number of items in the queue.
 	 */
 	public get size(): number {
-		return this.items.length;
+		return this.items.length - this.head;
 	}
 
 	/**
 	 * Check if the queue is empty.
 	 */
 	public get isEmpty(): boolean {
-		return this.items.length === 0;
+		return this.size === 0;
 	}
 
 	/**
@@ -36,7 +37,17 @@ export class Queue<T> {
 	 * @returns The item at the front of the queue, or undefined if the queue is empty
 	 */
 	public dequeue(): T | undefined {
-		return this.items.shift();
+		if (this.head >= this.items.length) {
+			return undefined;
+		}
+
+		const item = this.items[this.head++];
+		if (this.head >= 64 && this.head * 2 >= this.items.length) {
+			this.items = this.items.slice(this.head);
+			this.head  = 0;
+		}
+
+		return item;
 	}
 
 	/**
@@ -45,7 +56,7 @@ export class Queue<T> {
 	 * @returns The item at the front of the queue without removing it, or undefined if the queue is empty
 	 */
 	public peek(): T | undefined {
-		return this.items[0];
+		return this.items[this.head];
 	}
 
 	/**
@@ -53,6 +64,7 @@ export class Queue<T> {
 	 */
 	public clear(): void {
 		this.items = [];
+		this.head  = 0;
 	}
 
 	/**
@@ -61,7 +73,7 @@ export class Queue<T> {
 	 * @returns An iterator for the items in the queue
 	 */
 	public [Symbol.iterator](): Iterator<T> {
-		return this.items[Symbol.iterator]();
+		return this.toArray()[Symbol.iterator]();
 	}
 
 	/**
@@ -70,6 +82,6 @@ export class Queue<T> {
 	 * @returns An array containing all items in the queue in order
 	 */
 	public toArray(): T[] {
-		return [...this.items];
+		return this.items.slice(this.head);
 	}
 }
