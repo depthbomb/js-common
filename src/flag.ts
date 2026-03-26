da20133953bae9fb0c36edd5f11ee5c00e70bec6
depthbomb <depthbomb@users.noreply.github.com>
2026-03-26 10:23:27 -0500

    feat: add resettable-value and flag

---
+++ src/flag.ts
@@ -0,0 +1,32 @@
+import { ResettableValue } from './resettable-value';
+
+export class Flag extends ResettableValue<boolean> {
+	public constructor(value: boolean = false) {
+		super(value);
+	}
+
+	public get isTrue() {
+		return this.value === true;
+	}
+
+	public get isFalse() {
+		return !this.isTrue;
+	}
+
+	public override set(value: boolean) {
+		super.set(value);
+	}
+
+	public setTrue() {
+		this.value = true;
+	}
+
+	public setFalse() {
+		this.value = false;
+	}
+
+	public toggle() {
+		this.value = !this.value;
+	}
+
+}
