91c2e40e4471b4f55867b841f5e89be4551c2616
depthbomb <depthbomb@users.noreply.github.com>
2026-04-09 09:36:47 -0500

    refactor: simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,13 @@
 export * from './atomic';
 export * from './collections';
 export * from './date';
-export { deprecate, pipe } from './functional';
+export * from './functional';
 export * from './guards';
 export * from './number';
 export * from './promise';
 export * from './random';
 export * from './result';
-export { Flag, ResettableValue } from './state';
+export * from './state';
 export * from './timing';
-export { assume, cast, typedEntries } from './typing';
-export type {
-	Arrayable,
-	Awaitable,
-	AwaitableFn,
-	Brand,
-	Class,
-	DeepPartial,
-	DeepReadonly,
-	Fn,
-	JsonArray,
-	JsonObject,
-	JsonPrimitive,
-	JsonValue,
-	Maybe,
-	NonEmptyArray,
-	NonNullableValues,
-	Nullable,
-	OptionalKeys,
-	Prettify,
-	Primitive,
-	RequiredKeys,
-	ValueOf
-} from './typing';
+export * from './typing';
 export * from './url';
